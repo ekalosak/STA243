beffd8d50fb378e4f5c930c42619ca145faef3fa
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-05-29 15:02:02 -0700

    penalized regression spline calculation working and cross validation working

--- hw5/hw5.R
+++ hw5/hw5.R
@@ -46,7 +46,25 @@ for(j in 1:J){
 
 
 ## Select lambda for each method
-# TODO
+
+# Find solution matrix K in \hat{f}_\lambda = (I + \lambda K)^-1 Y
+h = knots[2] - knots[1]
+D = matrix(0, nrow = n-2, ncol = n)
+W = matrix(0, nrow = n-2, ncol = n-2)
+
+for(i in 1:(n-2)){
+    D[i,i] = 1/h
+    D[i, i+1] = -1/h-1/h
+    D[i, i+2] = 1/h
+    if(i>1){
+        W[i-1, i] = h/6
+        W[i, i-1] = h/6
+    }
+    W[i, i] = 2*h/3
+}
+K = t(D)%*%solve(W)%*%D
+
+# CV
 lam0 = 0.0001
 lam1 = 1.5
 lam2 = 2
@@ -54,7 +72,7 @@ lam3 = 2.5
 
 # ## Fit the spline using the true data noise_df$yt
 model_cv = smooth.spline(   # regular CV
-    x=xis, y=noise_df$yt, nknots=k, lambda=lam0)
+    x=xis, y=noise_df$yt, all.knots=FALSE, nknots=k, lambda=lam0)
 # model_gcv = smooth.spline(  # generalized CV
 #     x=xis, y=yis, nknots=k, lambda=lam1)
 # model_el = smooth.spline(   # expected loss
@@ -71,3 +89,16 @@ plt_pdf = ggplot(data=plot_df) +
     geom_line(aes(x=x, y=value, color=variable)) #+
     # geom_point(aes(x=x, y=y1)) + #NOTE only showing one noise schedule
     # labs(x="X", y="Y", title="Raw data and true f(x)")
+
+#
+# Using the built in penalized regression spline `R` function, we find
+# 
+# ```{r}
+# builtin_cv = smooth.spline(x=xis, y=ex_ys, nknots=k, cv=TRUE)
+# ```
+# 
+# a cross validated $\lambda_{CV}^\star=`r builtin_cv$lambda`$. Notice that
+# $\lambda_{CV}$ and $\lambda_{CV}^\star$ may be different due to different
+# implementations of cross validation proceedures, the imprecise, unsettled
+# terminology in the spline literature, and the stochastic noise generation
+# proceedures.
