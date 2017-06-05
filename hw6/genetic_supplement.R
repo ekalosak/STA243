e5f89f160845625fa29fcc0963abf3a9539073d7
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-06-05 00:33:12 -0700

    renamed function

--- hw6/genetic_supplement.R
+++ hw6/genetic_supplement.R
@@ -1,7 +1,7 @@
 ## Supplemental code for homework 6
 # Author: Eric Kalosa-Kenyon
 
-truefunction<-function(x){
+model1<-function(x){
     t <- c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
     h <- c(4, -5, 3, -4, 5, -4.2, 2.1, 4.3, -3.1, 2.1, -4.2)
     temp <- 0
