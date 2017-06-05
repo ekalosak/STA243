de47d7e07881a55ecdb65dcd3e68dd69117c9174
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-06-04 22:13:51 -0700

    solved 1.c

--- hw6/hw6.R
+++ hw6/hw6.R
@@ -2,3 +2,20 @@
 library(ggplot2)
 library(stats)
 library(reshape)
+
+## Parameterize
+n = 50
+t = 3
+B = 5000
+
+## Generate dataset
+xs = runif(n, 0, t)
+
+## Generate bootstrap samples and calculate var(\hat{\theta})
+
+# generate bootstrap
+bs_raw = sample(xs, size=n*B, replace=TRUE)
+bs = matrix(data=bs_raw, nrow=B, ncol=n)
+
+# calculate var
+
