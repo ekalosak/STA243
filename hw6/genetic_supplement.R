c3671bcd7093204d8c48e1eab721ff7494c20f56
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2017-06-05 00:27:35 -0700

    added genetic programming supmelemtal codefor homework 6

---
+++ hw6/genetic_supplement.R
@@ -0,0 +1,12 @@
+## Supplemental code for homework 6
+# Author: Eric Kalosa-Kenyon
+
+truefunction<-function(x){
+    t <- c(0.1, 0.13, 0.15, 0.23, 0.25, 0.4, 0.44, 0.65, 0.76, 0.78, 0.81)
+    h <- c(4, -5, 3, -4, 5, -4.2, 2.1, 4.3, -3.1, 2.1, -4.2)
+    temp <- 0
+    for(i in 1:11) {
+        temp <- temp + h[i]/2 * (1 + sign(x - t[i]))
+    }
+    return(temp)
+}
